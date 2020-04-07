26cb95bb4171bc9473269748ebc09ed4a1d4a003
Matt Cowgill <mcowgill@8140D-152935-M.local>
2020-04-07 17:11:29 +1000

    export function

--- R/richlegend.R
+++ R/richlegend.R
@@ -1,66 +1,14 @@
-
-#' @importFrom ggtext geom_richtext GeomRichtext GeomRichText
-
-richlegend_text <- function(plot, aes_type = "fill") {
-
-  p <- ggplot2::ggplot_build(plot)
-
-  scales <- p$plot$scales$scales
-
-  matches_aes_type <- function(scale) {
-    aes <- scale[["aesthetics"]]
-    identical(aes, aes_type)
-  }
-
-  scales_match_type <- purrr::map_lgl(.x = scales, matches_aes_type)
-
-  matching_scale <- scales[which(scales_match_type)]
-
-  if(length(matching_scale) > 1) {
-    stop("More than one scale matches ", aes_type)
-  }
-
-  if(length(matching_scale) < 1) {
-    stop("No scale matches ", aes_type)
-  }
-
-  pal <- matching_scale[[1]][["palette.cache"]]
-
-  vars <- matching_scale[[1]][["range"]][["range"]]
-
-  text_legend <- paste0("<span style='color:",
-                        pal,
-                        "'>",
-                        vars,
-                        "</span>",
-                        collapse = "<br>")
-
-  text_legend
-
-}
-
-
-richlegend_call <- function(plot,
-                            x, y,
-                            aes_type = "fill",
-                            label.padding = unit(0, "lines"),
-                            label.size = 0,
-                            ...) {
-
-  text_legend <- richlegend_text(plot, aes_type = aes_type)
-
-  make_layer(geom = "richtext",
-             x = x,
-             y = y,
-             label = text_legend,
-             params = c(list(label.padding = label.padding,
-                             label.size = label.size),
-                        ...)
-  )
-
-}
-
-
+#' Add a rich legend to a plot
+#'
+#' @param plot plot
+#' @param aes_type "fill" or "colour"
+#' @param x x
+#' @param y y
+#' @param label.padding label.padding
+#' @param label.size label.size
+#' @param ... additional parameters passed to `ggtext::geom_richtext()`
+#' @name richlegend
+#' @export
 richlegend <- function(plot,
                        aes_type = "fill",
                        x, y,
@@ -79,7 +27,3 @@ richlegend <- function(plot,
   eval(legend_call)
 
 }
-
-
-
-
---
+++ R/richlegend_helpers.R
@@ -0,0 +1,72 @@
+#' @importFrom ggtext geom_richtext GeomRichtext GeomRichText
+
+richlegend_text <- function(plot, aes_type = "fill") {
+
+  p <- ggplot2::ggplot_build(plot)
+
+  scales <- p$plot$scales$scales
+
+  matches_aes_type <- function(scale) {
+    aes <- scale[["aesthetics"]]
+    identical(aes, aes_type)
+  }
+
+  scales_match_type <- purrr::map_lgl(.x = scales, matches_aes_type)
+
+  matching_scale <- scales[which(scales_match_type)]
+
+  if(length(matching_scale) > 1) {
+    stop("More than one scale matches ", aes_type)
+  }
+
+  if(length(matching_scale) < 1) {
+    stop("No scale matches ", aes_type)
+  }
+
+  pal <- matching_scale[[1]][["palette.cache"]]
+
+  vars <- matching_scale[[1]][["range"]][["range"]]
+
+  text_legend <- paste0("<span style='color:",
+                        pal,
+                        "'>",
+                        vars,
+                        "</span>",
+                        collapse = "<br>")
+
+  text_legend
+
+}
+
+
+richlegend_call <- function(plot,
+                            x, y,
+                            aes_type = "fill",
+                            label.padding = unit(0, "lines"),
+                            label.size = 0,
+                            facet_vars = NULL,
+                            facet_levels = NULL,
+                            ...) {
+
+  text_legend <- richlegend_text(plot, aes_type = aes_type)
+
+
+  call(
+    "geom_richtext",
+    x = x,
+    y = y,
+    label = text_legend,
+    params = c(
+      list(label.padding = label.padding,
+           label.size = label.size),
+      ...
+    )
+  )
+
+}
+
+
+
+
+
+
